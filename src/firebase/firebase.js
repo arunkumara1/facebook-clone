d80bcf2d4e1a7c359611e410b6f569786c7d0331
arunkumar.a1 <arunkumar.a1>
2020-09-02 17:47:06 +0530

    refs # start  commit

---
+++ src/firebase/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCs4dgcQFbfZ3-4m6pFox6if6mupF0mhwM",
+    authDomain: "fabebook-clone-dd97c.firebaseapp.com",
+    databaseURL: "https://fabebook-clone-dd97c.firebaseio.com",
+    projectId: "fabebook-clone-dd97c",
+    storageBucket: "fabebook-clone-dd97c.appspot.com",
+    messagingSenderId: "946978127814",
+    appId: "1:946978127814:web:50d1c8933e31b2d31bfcc7",
+    measurementId: "G-K0Y6PQ56P7"
+  };
+
+  
